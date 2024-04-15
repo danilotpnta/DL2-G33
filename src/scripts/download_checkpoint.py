6e55ce6e1451c7b0e59f345f055dd2e0534f879d
Danilo Toapanta <70687643+danilotpnta@users.noreply.github.com>
2024-04-16 00:39:07 +0200

    update dir

--- src/scripts/download_checkpoint.py
+++ src/scripts/download_checkpoint.py
@@ -1,5 +1,6 @@
 import subprocess
 import sys
+import os
 
 # Install the package using pip
 subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'gdown'])
@@ -7,5 +8,8 @@ subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'gdown'])
 import gdown
 
 url = 'https://drive.google.com/uc?id=1ARiB5RkSsWmAB_8mqWnwDF8ZKTtFwsjl'
-output = '../data/checkpoints/sam-med2d_b.pth'
+checkpoint_dir = '../data/checkpoints/'
+os.makedirs(checkpoint_dir, exist_ok=True)
+
+output = checkpoint_dir + 'sam-med2d_b.pth'
 gdown.download(url, output, quiet=False)
