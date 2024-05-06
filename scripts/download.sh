139b80f0f73cb6330627746725c12bbc22e94d43
mkrastev <matey_krastev2@hotmail.com>
2024-05-06 15:30:56 +0300

    Added download script

---
+++ scripts/download.sh
@@ -0,0 +1,34 @@
+save_folder=$(pwd)/datasets
+
+if [ ! -d $save_folder ]; then
+    GIT_LFS_SKIP_SMUDGE=1 git clone https://huggingface.co/datasets/GoodBaiBai88/M3D-Seg $save_folder
+fi
+
+# Dataset should follow the format of 0000-0018
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <dataset_number>"
+    exit 1
+fi
+
+if [[ $1 != "24_1" && $1 != "24_2" && ($1 -lt 0 || $1 -gt 23) ]]; then
+    echo "Dataset number should be between 0 and 24(_1, _2)."
+    exit 1
+fi
+
+
+if [[ $1 == "24_1" ]] || [[ $1 == "24_2" ]]; then
+    dataset="00$1"
+else
+    dataset=$(printf "%04d" $1)
+fi
+
+cd "$save_folder/M3D_Seg"
+if [ ! -f $save_folder/$dataset ]; then
+    echo "Downloading $dataset ..."
+    git lfs pull --include="$dataset.zip"
+    echo "Unzipping $dataset ..."
+    unzip $save_folder/M3D_Seg/$dataset
+    echo "Done!"
+else
+    echo "$dataset already exists."
+fi
